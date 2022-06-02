c2fcbc58ee290ade010c5ee5d7244475a517b5e9
Brooke Chalmers <breq@breq.dev>
2022-06-01 20:29:20 -0600

    zsh-syntax-highlighting

--- .zshrc
+++ .zshrc
@@ -46,7 +46,7 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # ohmyzsh plugins
-plugins=(git)
+plugins=(git zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
--- install.zsh
+++ install.zsh
@@ -1,5 +1,19 @@
 REPO_ABSOLUTE_PATH=$(pwd)
 
+# download plugins
+if [ -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]
+then
+else
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ -s ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]
+then
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+# symlink configs
 rm -f ~/.zshrc
 ln -s $REPO_ABSOLUTE_PATH/.zshrc ~/.zshrc
 
