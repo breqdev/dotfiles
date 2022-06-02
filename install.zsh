8b8e8eaf2bd0836281163ac6b9be129cbeeb4869
Brooke Chalmers <breq@breq.dev>
2022-06-01 19:41:09 -0600

    broken zsh install script

--- install.zsh
+++ install.zsh
@@ -1,7 +1,7 @@
 REPO_ABSOLUTE_PATH=$(pwd)
 
 rm -f ~/.zshrc
-ln -s $REPO_ABSOLUTE_PATH/zshrc ~/.zshrc
+ln -s $REPO_ABSOLUTE_PATH/.zshrc ~/.zshrc
 
 rm -f ~/.p10k.zsh
 ln -s $REPO_ABSOLUTE_PATH/.p10k.zsh ~/.p10k.zsh
